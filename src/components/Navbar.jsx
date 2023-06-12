9db36440312e95743d86dd0db09d24444c67441e
SimonGideon <simongideon918@gmail.com>
2023-06-12 19:55:40 +0300

    Restructure Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,34 @@
 import { NavLink } from 'react-router-dom';
-import Logo from './images/house.jpg';
-import './Navbar.css';
 
-const visitPageAction = (e) => {
-  const links = document.querySelectorAll('.outer .inner-2 ul li a');
-  for (let i = 0; i < links.length; i += 1) {
-    if (links[i] === e.target) {
-      e.target.classList.add('active');
-    } else {
-      links[i].classList.remove('active');
-    }
-  }
-};
+const Navbar = () => {
+  const links = [
+    { path: '/', text: 'Houses' },
+    { path: '/reserve', text: 'Reserve' },
+    { path: '/myReservations', text: 'My Reservations' },
+    { path: '/addHouse', text: 'Add House' },
+    { path: '/deleteHouse', text: 'Delete House' },
+  ];
 
-const Navbar = () => (
-  <div className="outer">
-    <div className="inner-1">
-      <NavLink to="/" className="logo">
-        <img src={Logo} alt="logo" width="80" height="80" />
-        <p>House Renting</p>
-      </NavLink>
-    </div>
-    <div className="inner-2">
+  return (
+    <nav>
+      <div>
+        <h1>Alpha Reservation</h1>
+      </div>
       <ul>
-        <li><NavLink to="/" onClick={visitPageAction}>Houses</NavLink></li>
-        <li><NavLink to="/reserve" onClick={visitPageAction}>Reservations</NavLink></li>
-        <li><NavLink to="/myReservations" onClick={visitPageAction}>My Reservations</NavLink></li>
-        <li><NavLink to="/addHouse" onClick={visitPageAction}>Add House</NavLink></li>
-        <li><NavLink to="/deleteHouse" onClick={visitPageAction}>Delete House</NavLink></li>
-        {/* <li className="vertical-line" />
-        <li><NavLink to="/my-profile" onClick={visitPageAction}>My Profile</NavLink></li> */}
+        {links.map((link) => (
+          <li key={link.text}>
+            <NavLink
+              to={link.path}
+              style={({ isActive }) => ({
+                textDecoration: isActive ? 'underline' : undefined,
+              })}
+            >
+              {link.text}
+            </NavLink>
+          </li>
+        ))}
       </ul>
-    </div>
-  </div>
-);
+    </nav>
+  );
+};
 export default Navbar;
