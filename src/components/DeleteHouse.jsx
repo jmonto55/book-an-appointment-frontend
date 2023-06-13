2163433dbc831eb5a0e45f7ae71f3fbdc366eb9c
SimonGideon <simongideon918@gmail.com>
2023-06-13 15:05:11 +0300

    fetch house items

--- src/components/DeleteHouse.jsx
+++ src/components/DeleteHouse.jsx
@@ -1,7 +1,31 @@
-const DeleteHouse = () => (
-  <div>
-    Actions to delete a House.
-  </div>
-);
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchHouses } from '../redux/houses/housesSlice';
+
+const DeleteHouse = () => {
+  const dispatch = useDispatch();
+  const { housesList } = useSelector((store) => store.houses);
+
+  useEffect(() => {
+    dispatch(fetchHouses());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <ul>
+        {housesList.map((house) => (
+          <li key={house.id}>
+            <h1>{house.name}</h1>
+            <img src={house.photo} alt={house.name} />
+            <p>{house.description}</p>
+            <p>{house.price}</p>
+            <p>{house.address}</p>
+            <p>{house.city}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default DeleteHouse;
