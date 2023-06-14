5bd3f77ca76f07aeb77bc979731cfa383a16d2d1
darkxdev <bryanfernandohurtado@gmail.com>
2023-06-14 14:03:42 -0500

    Reset form when submitting

--- src/components/AddHouse.jsx
+++ src/components/AddHouse.jsx
@@ -4,22 +4,22 @@ import { createHouse } from '../redux/houses/housesSlice';
 
 const AddHouse = () => {
   const dispatch = useDispatch();
-  const [formData, setFormData] = useState([
+  const initialFormData = [
     { name: 'name', value: '' },
     { name: 'address', value: '' },
     { name: 'description', value: '' },
     { name: 'city', value: '' },
     { name: 'photo', value: '' },
     { name: 'night_price', value: 0 },
-  ]);
+  ];
+  const [formData, setFormData] = useState(initialFormData);
 
   const capitalize = (string) => string.charAt(0).toUpperCase() + string.slice(1);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevFormData) => prevFormData.map((field) => (
-      field.name === name ? { ...field, value } : field
-    )));
+      field.name === name ? { ...field, value } : field)));
   };
 
   const handleSubmit = (e) => {
@@ -29,13 +29,16 @@ const AddHouse = () => {
       {},
     );
     dispatch(createHouse(houseData));
-    setFormData((prevFormData) => prevFormData.map((field) => (field.name === 'night_price' ? { ...field, value: 0 } : field)));
+    setFormData(initialFormData);
   };
 
   return (
     <div className="p-8 h-screen" style={{ backgroundColor: '#96bf01' }}>
       <h2 className="font-black uppercase mx-auto text-center text-2xl text-white">ADD A NEW HOUSE</h2>
-      <form onSubmit={handleSubmit} className="flex flex-col items-end container mx-auto border px-6 py-8 max-w-md mt-6 bg-gray-100 rounded-md">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col items-end container mx-auto border px-6 py-8 max-w-md mt-6 bg-gray-100 rounded-md"
+      >
         {formData.map((field) => (
           <input
             key={field.name}
@@ -44,17 +47,20 @@ const AddHouse = () => {
             placeholder={capitalize(field.name)}
             value={field.value}
             onChange={handleChange}
-            className="mt-2 w-full p-2 border
-            border-slate-300 rounded text-sm shadow-sm
-            focus:outline-none focus:border-sky-500
-            focus:ring-1 focus:ring-sky-500"
+            className="mt-2 w-full p-2 border border-slate-300 rounded text-sm shadow-sm
+              focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
+            required
+            minLength={field.name === 'name' || field.name === 'address' || field.name === 'city' ? 3 : undefined}
+            maxLength={field.name === 'name' || field.name === 'city' ? 16 : undefined}
+            min={field.name === 'night_price' ? 0 : undefined}
+            step={field.name === 'night_price' ? 1 : undefined}
+            pattern={field.name === 'night_price' ? '[0-9]*' : undefined}
           />
         ))}
         <button
           type="submit"
-          className="rounded-full bg-yellow-400 px-6 py-2 mt-4
-          color text-white font-bold w-min"
-          style={{ 'background-color': '#97BF0F' }}
+          className="rounded-full bg-yellow-400 px-6 py-2 mt-4 color text-white font-bold w-min"
+          style={{ backgroundColor: '#97BF0F' }}
         >
           SUBMIT
         </button>
