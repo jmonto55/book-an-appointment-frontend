fa83ce7628379ab57312859c56ce83deec55fda0
jmonto55 <jmonto55@gmail.com>
2023-06-14 17:10:31 -0500

    center add house form and apply background image

--- src/components/AddHouse.jsx
+++ src/components/AddHouse.jsx
@@ -50,43 +50,47 @@ const AddHouse = () => {
   }, []);
 
   return (
-    <div className="p-8 h-screen" style={{ backgroundColor: '#96bf01' }}>
-      <h2 className="font-black uppercase mx-auto text-center text-2xl text-white">ADD A NEW HOUSE</h2>
-      <form
-        onSubmit={handleSubmit}
-        className="flex flex-col items-end container mx-auto border px-6 py-8 max-w-md mt-6 bg-gray-100 rounded-md"
+    <div className="w-full bg-center bg-cover" style={{ backgroundImage: 'url(https://rb.gy/k7lc1)' }}>
+      <div
+        className="p-8 h-screen w-full flex flex-col justify-center bg-lime bg-opacity-80"
       >
-        {formData.map((field) => (
-          <input
-            key={field.name}
-            type={field.name === 'night_price' ? 'number' : 'text'}
-            name={field.name}
-            placeholder={field.name === 'night_price' ? 'Price per Night' : capitalize(field.name)}
-            value={field.value}
-            onChange={handleChange}
-            className="mt-2 w-full p-2 border border-slate-300 rounded text-sm shadow-sm
+        <h2 className="font-black uppercase mx-auto text-center text-2xl text-white">ADD A NEW HOUSE</h2>
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col items-end container mx-auto border px-6 py-8 max-w-md mt-6 bg-gray-100 rounded-md"
+        >
+          {formData.map((field) => (
+            <input
+              key={field.name}
+              type={field.name === 'night_price' ? 'number' : 'text'}
+              name={field.name}
+              placeholder={field.name === 'night_price' ? 'Price per Night' : capitalize(field.name)}
+              value={field.value}
+              onChange={handleChange}
+              className="mt-2 w-full p-2 border border-slate-300 rounded text-sm shadow-sm
               focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
-            required
-            minLength={field.name === 'name' || field.name === 'address' || field.name === 'city' ? 3 : undefined}
-            maxLength={field.name === 'name' || field.name === 'city' ? 16 : undefined}
-            min={field.name === 'night_price' ? 0 : undefined}
-            step={field.name === 'night_price' ? 1 : undefined}
-            pattern={field.name === 'night_price' ? '[0-9]*' : undefined}
-          />
-        ))}
-        {isSubmitted && (
+              required
+              minLength={field.name === 'name' || field.name === 'address' || field.name === 'city' ? 3 : undefined}
+              maxLength={field.name === 'name' || field.name === 'city' ? 16 : undefined}
+              min={field.name === 'night_price' ? 0 : undefined}
+              step={field.name === 'night_price' ? 1 : undefined}
+              pattern={field.name === 'night_price' ? '[0-9]*' : undefined}
+            />
+          ))}
+          {isSubmitted && (
           <p ref={successMessageRef} className="text-green-500 mt-2">
             Successfully created!
           </p>
-        )}
-        <button
-          type="submit"
-          className="rounded-full bg-yellow-400 px-6 py-2 mt-4 color text-white font-bold w-min"
-          style={{ backgroundColor: '#97BF0F' }}
-        >
-          SUBMIT
-        </button>
-      </form>
+          )}
+          <button
+            type="submit"
+            className="rounded-full bg-yellow-400 px-6 py-2 mt-4 color text-white font-bold w-min"
+            style={{ backgroundColor: '#97BF0F' }}
+          >
+            SUBMIT
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
