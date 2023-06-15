68e5a455f86ce5886bb4a7fc11f83ff1297d4bd7
jmonto55 <jmonto55@gmail.com>
2023-06-15 15:15:57 -0500

    Fix the backdrop filter as requested by team review

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -26,17 +26,17 @@ const Splash = () => {
   }, []);
 
   return (
-    <div className="w-screen h-screen bg-mustard">
+    <div className="w-screen h-screen bg-center bg-no-repeat" style={{ backgroundImage: `url(${houseImage})` }}>
       <div
-        style={{ backgroundImage: `url(${houseImage})` }}
-        className="w-full h-full flex items-center justify-center bg-center bg-no-repeat"
+        className="w-full h-full bg-mustard bg-opacity-80 flex flex-col items-center justify-center"
       >
-        <div className="flex flex-col">
-          <button type="button" className="border-2 border-t-0 border-l-0 border-white/25 backdrop-filter backdrop-blur-lg bg-opacity-80 shadow-md hover:bg-opacity-100 bg-lime rounded-full px-4 py-2 mt-6 color text-white flex justify-evenly items-center">
+        <h2 className="mb-12 uppercase text-4xl sm:text-5xl font-black text-white-100 tracking-widest font-ubuntu text-center">alpha  reservations</h2>
+        <div className="flex flex-col sm:flex-row gap-8">
+          <button type="button" className="text-lg border-2 border-t-0 border-l-0 border-white/25 backdrop-filter backdrop-blur-lg bg-opacity-80 shadow-md hover:bg-opacity-100 bg-lime rounded-full px-4 py-2 color text-white flex justify-evenly items-center">
             Log In
             <BiChevronRightCircle size={20} className="ml-4" />
           </button>
-          <button type="button" className="border-2 border-t-0 border-l-0 border-white/25 backdrop-filter backdrop-blur-lg bg-opacity-80 shadow-md hover:bg-opacity-100 bg-lime rounded-full px-5 py-2 mt-6 color text-white flex justify-evenly items-center">
+          <button type="button" className="text-lg border-2 border-t-0 border-l-0 border-white/25 backdrop-filter backdrop-blur-lg bg-opacity-80 shadow-md hover:bg-opacity-100 bg-lime rounded-full px-5 py-2 color text-white flex justify-evenly items-center">
             Sign Up
             <BiChevronRightCircle size={20} className="ml-4" />
           </button>
