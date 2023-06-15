49dccb5a8d2e4e293e935a42bec9acc8faae2659
SimonGideon <simongideon918@gmail.com>
2023-06-15 09:04:56 +0300

    Mobile design

--- src/components/DeleteHouse.jsx
+++ src/components/DeleteHouse.jsx
@@ -17,6 +17,7 @@ const DeleteHouse = () => {
       title: 'Are you sure?',
       text: 'Once deleted, you will not be able to recover this house!',
       icon: 'warning',
+      confirmButtonColor: '#96BF01',
       showCancelButton: true,
       confirmButtonText: 'Delete',
       cancelButtonText: 'Cancel',
@@ -24,28 +25,46 @@ const DeleteHouse = () => {
     }).then((result) => {
       if (result.isConfirmed) {
         dispatch(deleteHouse(houseId)).then(() => {
-          Swal.fire('Deleted!', 'The house has been deleted.', 'success');
+          Swal.fire('Deleted!', 'The house has been deleted.', 'success').then(() => {
+            window.location.reload();
+          });
         });
       }
     });
   };
 
   return (
-    <div>
-      <ul>
+    <div className="container mx-auto">
+      <ul className="flex flex-col  ">
         {housesList.map((house) => (
-          <li key={house.id}>
-            <h1>{house.name}</h1>
-            <img src={house.photo} alt={house.name} />
-            <p>{house.description}</p>
-            <p>{house.price}</p>
-            <p>{house.address}</p>
-            <p>{house.city}</p>
-            <button type="button" onClick={() => handleDelete(house.id)}>Delete</button>
-          </li>
+          <div key={house.id} className="bg-white shadow rounded-lg p-4 m-4 relative ">
+            <img className="w-full rounded-lg h-auto mb-4" src={house.photo} alt={house.name} />
+            <div className="absolute top-6 right-5 bg-[#FFA500] text-white p-1 rounded-lg">
+              <span className="font-bold">
+                $
+                {house.night_price}
+              </span>
+            </div>
+            <div className="flex flex-col justify-between">
+              <div>
+                <h1 className="uppercase text-xl font-bold mb-2">{house.name}</h1>
+                <p className="mb-2">{house.description}</p>
+                <p className="mb-2">{house.address}</p>
+                <p className="mb-2">{house.city}</p>
+                <button
+                  type="button"
+                  onClick={() => handleDelete(house.id)}
+                  className="bg-[#96BF01] hover:bg-[#FFA500] text-white rounded px-4 py-2 mt-4"
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
         ))}
       </ul>
     </div>
+
   );
 };
 
